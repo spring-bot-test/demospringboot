cf859805ae01d15c51f59bb05551946f50365143
Rambabu <rkrambabu3@gmail.com>
2024-08-22 15:26:39 +0530

    pushing to repo

--- versioning_script.sh
+++ versioning_script.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+# COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
-if [[ $COMMIT_MESSAGE =~ ^feat ]]; then
-    mvn versions:set -DnextSnapshot=true
-elif [[ $COMMIT_MESSAGE =~ ^fix ]]; then
-    mvn versions:set -DnextPatch=true
-fi
+# if [[ $COMMIT_MESSAGE =~ ^feat ]]; then
+#     mvn versions:set -DnextSnapshot=true
+# elif [[ $COMMIT_MESSAGE =~ ^fix ]]; then
+#     mvn versions:set -DnextPatch=true
+# fi
 
-mvn release:prepare release:perform
+# mvn release:prepare release:perform
